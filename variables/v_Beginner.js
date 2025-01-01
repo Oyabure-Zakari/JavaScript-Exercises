bd4e4dc4bc53d061289381cefe62b21fc3de5e10
Oyabure Zakari <oyabure205@gmail.com>
2025-01-01 11:10:15 +0100

    solved the beginner level exercise on variables

--- variables/v_Beginner.js
+++ variables/v_Beginner.js
@@ -1,16 +1,30 @@
-// Beginner Level
+// // Beginner Level
 
-// 1) Swapping Two Variables, Write a program that swaps the values of two variables without using a third variable.
-//Example:
-let a = 5;
-let b = 10;
-// Your code here
-[a, b] = [b, a];
-console.log(a, b); // Output: 10, 5
+// // 1) Swapping Two Variables, Write a program that swaps the values of two variables without using a third variable.
+// //Example:
+// let a = 5;
+// let b = 10;
+// // Your code here
+// [a, b] = [b, a];
+// console.log(a, b); // Output: 10, 5
 
 // 2) Simple Arithmetic with Variables, Create three variables: num1, num2, and result. Assign values to num1 and num2, then use arithmetic operations (+, -, *, /) to calculate the sum, difference, product, and quotient, storing the results in result.
 //Example:
 let num1 = 20;
 let num2 = 4;
 // Your code here
-// console.log(result); // Output depends on the operation performed
+//sum
+let sum = num1 + num2;
+console.log(sum);
+
+//difference
+let difference = num1 - num2;
+console.log(difference);
+
+//product
+let product = num1 * num2;
+console.log(product);
+
+//quotient
+let quotient = num1 / num2;
+console.log(quotient);
